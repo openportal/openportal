53fea330674fd4716a50a9e2c937cce9ac10ba45
Robert Hintz <meowmixer@meowmixer-MS-7750.(none)>
2013-10-03 17:53:07 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Openportal::Application.config.secret_key_base = '83723ee23aeb3c511e20fc4b7ce1170e59b7be7f00467de5b0c6c7316d9ba3d9fd823ce086c14ed1c09a0703edeb34ec75747b3549ba4536b00b69148ce2d9da'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Openportal::Application.config.session_store :cookie_store, key: '_openportal_session'
